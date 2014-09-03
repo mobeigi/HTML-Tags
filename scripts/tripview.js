2d0d30ab2a853dd32a6fdcfe7af1e8c55316e447
mohammadg <webmaster@mohammadg.com>
2014-09-03 12:16:22 +1000

    Teemo on Map

--- scripts/tripview.js
+++ scripts/tripview.js
@@ -2,22 +2,107 @@
 * Trip View
 */
 
-var map;
+// This example creates a custom overlay called ImageOverlay, containing
+// a U.S. Geological Survey (USGS) image of the relevant area on the map.
+
+// Set the custom overlay object's prototype to a new instance
+// of OverlayView. In effect, this will subclass the overlay class.
+// Note that we set the prototype to an instance, rather than the
+// parent class itself, because we do not wish to modify the parent class.
+
+var overlay;
+ImageOverlay.prototype = new google.maps.OverlayView();
+
+// Initialize the map and the custom overlay.
+
 function initialize() {
   var mapOptions = {
-    zoom: 18,
-    center: new google.maps.LatLng(37.78313383212, -122.4039494991302)
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+    zoom: 11,
+    center: new google.maps.LatLng(62.323907, -150.109291),
+   };
 
-  var bounds = {
-    17: [[20969, 20970], [50657, 50658]],
-    18: [[41939, 41940], [101315, 101317]],
-    19: [[83878, 83881], [202631, 202634]],
-    20: [[167757, 167763], [405263, 405269]]
-  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+  var swBound = new google.maps.LatLng(62.303907, -150.159291);
+  var neBound = new google.maps.LatLng(62.323907, -150.109291);
+  var bounds = new google.maps.LatLngBounds(swBound, neBound);
+
+  // The photograph to be placed over the map
+  var srcImage = 'http://fc07.deviantart.net/fs71/f/2012/165/c/8/teemo_skin_by_yurixy-d53itci.png';
+  
+  // Create object to contain the overlay image, the bounds of the image and a reference to the map
+  overlay = new ImageOverlay(bounds, srcImage, map);
 }
+
+/** @constructor */
+function ImageOverlay(bounds, image, map) {
+
+  // Initialize all properties.
+  this.bounds_ = bounds;
+  this.image_ = image;
+  this.map_ = map;
+
+  // Define a property to hold the image's div. We'll
+  // actually create this div upon receipt of the onAdd()
+  // method so we'll leave it null for now.
+  this.div_ = null;
+
+  // Explicitly call setMap on this overlay.
+  this.setMap(map);
+}
+
+/**
+ * onAdd is called when the map's panes are ready and the overlay has been
+ * added to the map.
+ */
+ImageOverlay.prototype.onAdd = function() {
+
+  var div = document.createElement('div');
+  div.style.borderStyle = 'none';
+  div.style.borderWidth = '0px';
+  div.style.position = 'absolute';
+
+  // Create the img element and attach it to the div.
+  var img = document.createElement('img');
+  img.src = this.image_;
+  img.style.width = '100%';
+  img.style.height = '100%';
+  img.style.position = 'absolute';
+  div.appendChild(img);
+
+  this.div_ = div;
+
+  // Add the element to the "overlayLayer" pane.
+  var panes = this.getPanes();
+  panes.overlayLayer.appendChild(div);
+};
+
+ImageOverlay.prototype.draw = function() {
+
+  // We use the south-west and north-east
+  // coordinates of the overlay to peg it to the correct position and size.
+  // To do this, we need to retrieve the projection from the overlay.
+  var overlayProjection = this.getProjection();
+
+  // Retrieve the south-west and north-east coordinates of this overlay
+  // in LatLngs and convert them to pixel coordinates.
+  // We'll use these coordinates to resize the div.
+  var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
+  var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+
+  // Resize the image's div to fit the indicated dimensions.
+  var div = this.div_;
+  div.style.left = sw.x + 'px';
+  div.style.top = ne.y + 'px';
+  div.style.width = (ne.x - sw.x) + 'px';
+  div.style.height = (sw.y - ne.y) + 'px';
+};
+
+// The onRemove() method will be called automatically from the API if
+// we ever set the overlay's map property to 'null'.
+ImageOverlay.prototype.onRemove = function() {
+  this.div_.parentNode.removeChild(this.div_);
+  this.div_ = null;
+};
 
 google.maps.event.addDomListener(window, 'load', initialize);
