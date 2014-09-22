a58eb9cc12820d5a89fd9863a25c7457fd9575cb
Jason Ng <jsnng@users.noreply.github.com>
2014-09-22 16:44:33 +1000

    .

--- includes/db/install.php
+++ includes/db/install.php
@@ -1,10 +1,11 @@
 <?php
 
-include_once('postgres.php')
-$host = "23.102.176.176";
-$username = "azureuser";
-$password = "YJ7dMgOCPSsM1dI";
-$database = "test";
+include_once('postgres.php');
+
+$host = '23.102.176.176';
+$username = 'azureuser';
+$password = 'RLSfTv3Ewx';
+$database = 'test';
 $port = 5432;
 
 $pg = new postgres();
@@ -20,8 +21,8 @@ CREATE TABLE trips (
 	trip_id varchar(10) SERIAL PRIMARY KEY,
 	name varchar(64) NOT NULL,
 	description varchar(256), 
-)'
-$pg->_pg_query($query)
+)';
+$pg->_pg_query($query);
 $query = '
 CREATE TABLE image_groups (
 	group_id varchar(10) SERIAL PRIMARY KEY, 
@@ -30,28 +31,28 @@ CREATE TABLE image_groups (
 	longitude float(10) NOT NULL,
 	latitude float(10) NOT NULL
 )';
-$pg->_pg_query($query)
+$pg->_pg_query($query);
 $query = '
 CREATE TABLE images (
 	image_id varchar(10) SERIAL PRIMARY KEY,
 	group_id varchar(10) REFERENCES image_groups(group_id) ON DELETE CASCADE,
 	description varchar(256),
 )';
-$pg->_pg_query($query)
+$pg->_pg_query($query);
 $query = '
 ALTER TABLE trips (
 	ADD cover_image varchar(10) REFERENCES images(image_id)
 )';
-$pg->_pg_query($query)
+$pg->_pg_query($query);
 $query = '
 ALTER TABLE image_groups (
 	ADD cover_image varchar(10) REFERENCES images(image_id)
 )';
-$pg->_pg_query($query)
+$pg->_pg_query($query);
 $query = '
 CREATE TABLE comments (
 	comment_id SERIAL PRIMARY KEY,
 	image_id varchar(10) REFERENCES images(image_id) ON DELETE CASCADE,	
 )';
-$pg->_pg_query($query)
+$pg->_pg_query($query);
 ?>
