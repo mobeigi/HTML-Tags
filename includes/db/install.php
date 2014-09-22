11da740a496b67db653e4de3607fbb350da92b3c
Jason Ng <jsnng@users.noreply.github.com>
2014-09-22 17:55:18 +1000

    .

--- includes/db/install.php
+++ includes/db/install.php
@@ -8,60 +8,45 @@ $password = 'RLSfTv3Ewx';
 $database = 'test';
 $port = 5432;
 
-if(!function_exists('pg_connect')) {
-	return false;
-}
 $pg = new postgres();
+
 $pg->_pg_connect($host, $username, $password, $database, $port);
 
-echo pg_last_error($pg->pg_connect_id);
 $pg->_pg_query('DROP TABLE IF EXISTS trips');
 $pg->_pg_query('DROP TABLE IF EXISTS image_groups');
 $pg->_pg_query('DROP TABLE IF EXISTS images');
-echo pg_last_error($pg->pg_connect_id);
+
 $query = '
 CREATE TABLE trips (
-	trip_id varchar(10) SERIAL PRIMARY KEY,
+	trip_id varchar(10) PRIMARY KEY,
 	name varchar(64) NOT NULL,
-	description varchar(256), 
+	description varchar(256)
 )';
 $pg->_pg_query($query);
-echo pg_last_error($pg->pg_connect_id);
 $query = '
 CREATE TABLE image_groups (
-	group_id varchar(10) SERIAL PRIMARY KEY, 
-	trip_id varchar(10) REFERENCES trips(trip_id) NOT NULL ON DELETE CASCADE,	
+	group_id varchar(10) PRIMARY KEY, 
+	trip_id varchar(10) REFERENCES trips(trip_id) NOT NULL,	
 	name varchar(64) NOT NULL,
 	longitude float(10) NOT NULL,
 	latitude float(10) NOT NULL
 )';
 $pg->_pg_query($query);
-echo pg_last_error($pg->pg_connect_id);
 $query = '
 CREATE TABLE images (
-	image_id varchar(10) SERIAL PRIMARY KEY,
-	group_id varchar(10) REFERENCES image_groups(group_id) ON DELETE CASCADE,
-	description varchar(256),
+	image_id varchar(10) PRIMARY KEY,
+	group_id varchar(10) REFERENCES image_groups(group_id),
+	description varchar(256)
 )';
 $pg->_pg_query($query);
-echo pg_last_error($pg->pg_connect_id);
-$query = '
-ALTER TABLE trips (
-	ADD cover_image varchar(10) REFERENCES images(image_id)
-)';
+$query = 'ALTER TABLE trips ADD COLUMN cover_image varchar(10) REFERENCES images(image_id)';
 $pg->_pg_query($query);
-echo pg_last_error($pg->pg_connect_id);
-$query = '
-ALTER TABLE image_groups (
-	ADD cover_image varchar(10) REFERENCES images(image_id)
-)';
+$query = 'ALTER TABLE image_groups ADD COLUMN cover_image varchar(10) REFERENCES images(image_id)';
 $pg->_pg_query($query);
-echo pg_last_error($pg->pg_connect_id);
 $query = '
 CREATE TABLE comments (
-	comment_id SERIAL PRIMARY KEY,
-	image_id varchar(10) REFERENCES images(image_id) ON DELETE CASCADE,	
+	comment_id varchar(10) PRIMARY KEY,
+	image_id varchar(10) REFERENCES images(image_id)
 )';
 $pg->_pg_query($query);
-echo pg_last_error($pg->pg_connect_id);
 ?>
