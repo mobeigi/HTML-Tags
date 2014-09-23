24f9cb957bb20ca4b8f8d62f98f2b68f4e40b854
mohammadg <webmaster@mohammadg.com>
2014-09-23 16:29:52 +1000

    Added test light box page

---
+++ testlightbox.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html >
+<html lang="en" ng-app="viewPageApp">
+<head>
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+ <title>TripTags</title>
+ <meta name="description" content="">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--      bootstrap css-->
+ <link rel="stylesheet" href="css/bootstrap.min.css">
+<!--      custom css -->
+ <link rel="stylesheet" href="css/main.css">
+<!--    fonts       -->
+ <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+ <!-- lightbox -->
+ <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+ <script src="js/lightbox.min.js"></script>
+
+ <link href="css/lightbox.css" rel="stylesheet" />
+ 
+  
+ </head>
+
+<body>
+<a href="img/concept.jpg" data-lightbox="image-1" data-title="Trip tag 2">Image #1</a>
+<a href="img/concept.jpg" data-lightbox="image-1" data-title="Trip tag 3">Image #1</a>
+<a href="img/concept.jpg" data-lightbox="image-1" data-title="Trip tag 4">Image #1</a>
+
+</body>
+
+</html>
