117b75c2896a53842c5d19ed746dc0cdbd085f7a
mohammadg <webmaster@mohammadg.com>
2014-09-27 03:31:40 +1000

    Added php header

--- index.php
+++ index.php
@@ -1,30 +1,9 @@
-
-    <!DOCTYPE html >
+<!DOCTYPE html >
 <html lang="en" ng-app="viewPageApp">
 <head>
- <meta charset="utf-8">
- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
- <title>TripTags</title>
- <meta name="description" content="">
- <meta name="viewport" content="width=device-width, initial-scale=1">
-<!--      bootstrap css-->
- <link rel="stylesheet" href="css/bootstrap.min.css">
-<!--      custom css -->
- <link rel="stylesheet" href="css/main.css">
-<!--    fonts       -->
- <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
-    
-    <!-- load tripviewer -->
-    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="maps/tripview.js"></script>
-
-    <!-- lightbox -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="js/lightbox.min.js"></script>
-    <script src="maps/imagegroupview.js"></script>
-    <link href="css/lightbox.css" rel="stylesheet" />
-
+<title>TripTags</title>
+<!-- include header -->
+<?php include_once "/includes/header.php"; ?>
 </head>
 
 <body>
