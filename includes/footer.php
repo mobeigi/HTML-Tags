4f4f34cba10f808e5158cf14cd4758f8b3979d5f
mohammadg <webmaster@mohammadg.com>
2014-09-27 03:52:07 +1000

    .

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer >
 <p>
-<a href="/>Home</a>&nbsp; | &nbsp;<a href="/">Tutorial</a>&nbsp; | &nbsp;<a href="/">Help</a>&nbsp; | &nbsp;<a href="/tos/">Terms of Service</a>&nbsp; | &nbsp;<a href="/contact/">Contact Us</a>&nbsp; | &nbsp;<a href="/about/">About Us</a>
+<a href="/">Home</a>&nbsp; | &nbsp;<a href="/">Tutorial</a>&nbsp; | &nbsp;<a href="/">Help</a>&nbsp; | &nbsp;<a href="/tos/">Terms of Service</a>&nbsp; | &nbsp;<a href="/contact/">Contact Us</a>&nbsp; | &nbsp;<a href="/about/">About Us</a>
 <br>
 <span>
 Copyright © 2014. HTMLTags Limited. All Rights Reserved.
