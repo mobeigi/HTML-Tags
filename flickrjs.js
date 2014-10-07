af752b04294c575ef9c94ab790ef05d22b726480
oringal <oringalyu@hotmail.com>
2014-10-08 10:56:56 +1100

    Revert "commit because errors"
    This reverts commit 4d4431df30377577b0695839dbd7dee24d593a4b.

--- flickrjs.js
+++ flickrjs.js
@@ -1,24 +1,29 @@
-var viewPageApp = angular.module('viewPageApp',[]); 
+var viewPageApp = angular.module('viewPageApp',[]);
 
 viewPageApp.controller('flickrController', function ($scope){
- //my API key for Flickr
-    var apiKey = 'ff6ec1c7727b287de5281ad2c74ace9d';
+    var apiKey = '9fe6d098f0ecd8855f5429ca8b9b4624';
     var album = '72157647658516722';
     var baseImages = [];
-	var flickrURL = 'https://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&api_key=' + apiKey + '&photoset_id=' + album + '&format=json&jsoncallback=?';
-    $.getJSON(flickrURL, function(data){
+
+    $.getJSON('https://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&api_key=' + apiKey + '&photoset_id=' + album + '&format=json&jsoncallback=?',
+function(data){
         //loop through each picture in the set
         $.each(data.photoset.photo, function(i,item){
             //contruct a URL for each image
             var photoURL = 'https://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '.jpg';
-            baseImages.push(photoURL); 
+            baseImages.push(photoURL);   
         });
     });
-   
-    //Function to display the correct number of photos
+    
+    $scope.images = baseImages.slice(1,4);
+});
+//    $scope.images.length = 3;
+    
+   // Function to display the correct number of photos
 //    $scope.listImages = function () {
 //        $scope.images = [];
-//        $scope.images = baseImages.slice();
+//        $scope.images = baseImages.slice;
+//       
 //        if($scope.numImages > baseImages.length){
 //            alert("Sorry I dont have that many photos");
 //            $scope.images.length = 0;
@@ -31,7 +36,5 @@ viewPageApp.controller('flickrController', function ($scope){
 //            alert("Please enter a whole number");
 //            $scope.images.length = 0;
 //        }
-//    }
-	
-	
-});
+
+//});
