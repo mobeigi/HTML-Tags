ebcac6e03ed2df551f974a81d55aac46e4055e2f
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-15 11:52:34 +1100

    changed auto-incrementing pk

--- install.php
+++ install.php
@@ -13,34 +13,34 @@ $password = 'RLSfTv3Ewx';
 $port = '5432';
 $pg->_pg_connect($host, $username, $password, $database, $port);
 
-if(!function_exists('pg_query')) {
-	return false;
-}
-$pg->_pg_query('DROP TABLE IF EXISTS users CASCADE');
 $pg->_pg_query('DROP TABLE IF EXISTS images CASCADE');
 $pg->_pg_query('DROP TABLE IF EXISTS image_groups CASCADE');
 $pg->_pg_query('DROP TABLE IF EXISTS trips CASCADE');
 $pg->_pg_query('DROP TABLE IF EXISTS comments CASCADE');
-
+// $pg_connect_id = pg_connect($connection_string);
+// $query = "INSERT INTO trips (trip_id, name, description, owner_id, cover_image) VAlUES ($1, $2, $3, $4, $5)"
+// pg_query_params($pg_connect_id, $query, $var1 ... $varn);
 $query = '
 CREATE TABLE trips (
-	trip_id varchar(10) PRIMARY KEY,
+	trip_id varchar(10) SERIAL PRIMARY KEY,
 	name varchar(64) NOT NULL,
 	description varchar(256)
 )';
 $pg->_pg_query($query);
+// INSERT INTO image_groups(group_id, trip_id, name, longitude, latitude, cover_image) VALUES ($?)
 $query = '
 CREATE TABLE image_groups (
-	group_id varchar(10) PRIMARY KEY, 
+	group_id varchar(10) SERIAL PRIMARY KEY, 
 	trip_id varchar(10) REFERENCES trips(trip_id) NOT NULL,	
 	name varchar(64) NOT NULL,
 	longitude float(10) NOT NULL,
 	latitude float(10) NOT NULL
 )';
 $pg->_pg_query($query);
+// INSERT INTO images(image_id, group_id, description) VALUES ($?)
 $query = '
 CREATE TABLE images (
-	image_id varchar(10) PRIMARY KEY,
+	image_id varchar(10) SERIAL PRIMARY KEY,
 	group_id varchar(10) REFERENCES image_groups(group_id),
 	description varchar(256)
 )';
@@ -49,15 +49,17 @@ $query = 'ALTER TABLE trips ADD COLUMN cover_image varchar(10) REFERENCES images
 $pg->_pg_query($query);
 $query = 'ALTER TABLE image_groups ADD COLUMN cover_image varchar(10) REFERENCES images(image_id)';
 $pg->_pg_query($query);
+// INSERT INTO comments (comment_id, image_id) VALUES ($?)
 $query = '
 CREATE TABLE comments (
-	comment_id varchar(10) PRIMARY KEY,
+	comment_id varchar(10) SERIAL PRIMARY KEY,
 	image_id varchar(10) REFERENCES images(image_id)
 )';
 $pg->_pg_query($query);
+// INSERT INTO users (user_id, user_email, user_name, user_password, profile_picture) VALUES ($?)
 $query = '
 CREATE TABLE users (
-	user_id varchar(10) PRIMARY KEY,
+	user_id varchar(10) SERIAL PRIMARY KEY,
 	user_email varchar(64),
 	user_name varchar(64),
 	user_password varchar(64),
