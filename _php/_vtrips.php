e4c55f79019c87bf3fec9d44a43913593a76c3d5
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-20 01:21:04 +1100

    .

--- _php/_vtrips.php
+++ _php/_vtrips.php
@@ -2,15 +2,16 @@
 include('_session.php');
 $result = $pg->_pg_query('SELECT * FROM trips');
 $rows = $pg->_pg_fetch_all($result);
-print '<table><tr>';
+$num_rows = $pg->_pg_num_rows($result);
+print '<table border=0 margin=0><tr>';
 print '<td>name</td>';
 print '<td>desc</td>';
 print '<td>privacy</td>';
-foreach ($rows as $i) {
+foreach ($num_rows as $i) {
 	print "<tr>";
-	print "<td>$row[$i][0]</td>";
-	print "<td>$row[$i][1]</td>";
-	print "<td>$row[$i][2]</td>";
+	print "<td>$rows[$i][0]</td>";
+	print "<td>$rows[$i][1]</td>";
+	print "<td>$rows[$i][2]</td>";
 	print "</td>";
 }
 print '</tr></table>';
