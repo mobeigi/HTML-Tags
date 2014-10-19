580bd047a4f5a48b68f0e736fa14b7d805fffd2e
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-20 02:03:48 +1100

    .

--- _php/_vtrips.php
+++ _php/_vtrips.php
@@ -6,4 +6,14 @@ $rows = $pg->_pg_fetch_all($result);
 $num_rows = $pg->_pg_num_rows($result);
 print $num_rows;
 print_r($rows);
+	
+for($i=0;$i!=$num_rows;$i++){
+	print '<tr><td>';
+	print $row[$i]['name'];
+	print '</td><td>'
+	print $row[$i]['description'];
+	print '</td><td>'
+	print $row[$i]['privacy'];
+	print '</td></tr>'
+}
 ?>
