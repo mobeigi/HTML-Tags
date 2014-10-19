4b413f45304b098b1429ffceab9f158bb2e94665
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-19 21:42:47 +1100

    .

--- _php/_create.php
+++ _php/_create.php
@@ -1,6 +1,7 @@
 <?php
 include ('_session.php');
-$trip_name = $_POST['trip_name'];
+
+if(empty($trip_name = $_POST['trip_name'])) return false;
 $trip_description = $_POST['trip_desc'];
 $trip_privacy = $_POST['trip_privacy'];
 
@@ -8,14 +9,11 @@ print $trip_name;
 print $trip_description;
 print $trip_privacy;
 
-if(empty($trip_name)) {
-	print 'variable $trip_name is empty';
-	return false;
-}
 $query = 'INSERT INTO trips (name, description, privacy) VALUES ($1, $2, $3)';
 $pg->_pg_transaction('begin');
 $result = $pg->_pg_query($query, $trip_name, $trip_description, $trip_privacy);
 if(!$result) $pg->transaction('rollback');
 $pg->_pg_transaction('commit');
+
 
 ?>
