04e4953cb554e241abcda3f4bfda046805224536
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-19 21:45:31 +1100

    .

--- _php/_create.php
+++ _php/_create.php
@@ -11,11 +11,11 @@ print $trip_description;
 print $trip_privacy;
 
 return false
-// 
+/* 
 $query = 'INSERT INTO trips (name, description, privacy) VALUES ($1, $2, $3)';
 $pg->_pg_transaction('begin');
 $result = $pg->_pg_query($query, $trip_name, $trip_description, $trip_privacy);
 if(!$result) $pg->transaction('rollback');
 $pg->_pg_transaction('commit');
-
+*/
 ?>
