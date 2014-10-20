95afc8cec0fa96c91a6f833037209f0124a4c678
mohammadg <webmaster@mohammadg.com>
2014-10-20 21:06:21 +1100

    .

--- functions/image_upload.php
+++ functions/image_upload.php
@@ -1,33 +1,35 @@
 <?php
-$file_formats = array("jpg", "png", "gif", "bmp"); // Set File format
-$filepath = "uploads/";
+$output_dir = "uploads/";
 
-if ($_POST['submitbtn']=="Submit") {
-  $name = $_FILES['imagefile']['name'];
-  $size = $_FILES['imagefile']['size'];
-
-   if (strlen($name)) {
-      $extension = substr($name, strrpos($name, '.')+1);
-      if (in_array($extension, $file_formats)) { 
-          if ($size < (2048 * 1024)) {
-             $imagename = md5(uniqid().time()).".".$extension;
-             $tmp = $_FILES['imagefile']['tmp_name'];
-             if (move_uploaded_file($tmp, $filepath . $imagename)) {
-		 echo '<img class="preview" alt="" src="'.$filepath.'/'. 
-			$imagename .'" />';
-	     } else {
-		 echo "Could not move the file.";
-	     }
-	  } else {
-		echo "Your image size is bigger than 2MB.";
+if ($_POST['submitbtn']=="Submit")
+{
+	$ret = array();
+  
+	$error =$_FILES["imagefile"]["error"];
+  
+	//You need to handle  both cases
+	//If Any browser does not support serializing of multiple files using FormData() 
+  
+	if(!is_array($_FILES["imagefile"]["name"])) //single file
+	{
+ 	 	$fileName = $_FILES["imagefile"]["name"];
+ 		move_uploaded_file($_FILES["imagefile"]["tmp_name"],$output_dir.$fileName);
+    	$ret[]= $fileName;
+	}
+	else  //Multiple files, file[]
+	{
+	  $fileCount = count($_FILES["imagefile"]["name"]);
+	  for($i=0; $i < $fileCount; $i++)
+	  {
+	  	$fileName = $_FILES["imagefile"]["name"][$i];
+		move_uploaded_file($_FILES["imagefile"]["tmp_name"][$i],$output_dir.$fileName);
+	  	$ret[]= $fileName;
 	  }
-       } else {
-	       echo "Invalid file format.";
-       }
-  } else {
-       echo "Please select image..!";
-  }
-exit();
-}
-
+	
+	}
+    echo json_encode($ret);
+ }
+ 
+ exit();
+ 
 ?>
