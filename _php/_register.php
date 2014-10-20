e090ac57aa8003870df8a661b4905ee46483671b
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-20 15:00:55 +1100

    ..

--- _php/_register.php
+++ _php/_register.php
@@ -3,8 +3,9 @@ include('_session.php');
 $page = $_POST['page']
 $username = $_POST['userEmail'];
 $password = $_POST['userPassword'];
-
+ if(!empty($username) || !empty($password)) {
 $query = 'insert into users (user_email,user_password) values ($1,$2)';
 $pg->_pg_query($query,$username,$password);
+}
 header("location: http://triptags.azurewebsites.net/$page");
 ?>
