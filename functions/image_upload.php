ccd885899e928ab7d98487a12ab07604ac292bdb
mohammadg <webmaster@mohammadg.com>
2014-10-20 19:35:07 +1100

    Trying another upload script

--- functions/image_upload.php
+++ functions/image_upload.php
@@ -1,29 +1,27 @@
 <?php
-$output_dir = "uploads/";
-if(isset($_FILES["myfile"]))
-{
-	$ret = array();
+if (isset($_POST['submit'])) {
+    $j = 0; //Variable for indexing uploaded image 
+    
+	$target_path = "uploads/"; //Declaring Path for uploaded images
+    for ($i = 0; $i < count($_FILES['file']['name']); $i++) {//loop to get individual element from the array
 
-	$error =$_FILES["myfile"]["error"];
-	//You need to handle  both cases
-	//If Any browser does not support serializing of multiple files using FormData() 
-	if(!is_array($_FILES["myfile"]["name"])) //single file
-	{
- 	 	$fileName = $_FILES["myfile"]["name"];
- 		move_uploaded_file($_FILES["myfile"]["tmp_name"],$output_dir.$fileName);
-    	$ret[]= $fileName;
-	}
-	else  //Multiple files, file[]
-	{
-	  $fileCount = count($_FILES["myfile"]["name"]);
-	  for($i=0; $i < $fileCount; $i++)
-	  {
-	  	$fileName = $_FILES["myfile"]["name"][$i];
-		move_uploaded_file($_FILES["myfile"]["tmp_name"][$i],$output_dir.$fileName);
-	  	$ret[]= $fileName;
-	  }
-	
-	}
-    echo json_encode($ret);
- }
- ?>
+        $validextensions = array("jpeg", "jpg", "png");  //Extensions which are allowed
+        $ext = explode('.', basename($_FILES['file']['name'][$i]));//explode file name from dot(.) 
+        $file_extension = end($ext); //store extensions in the variable
+        
+		$target_path = $target_path . md5(uniqid()) . "." . $ext[count($ext) - 1];//set the target path with a new name of image
+        $j = $j + 1;//increment the number of uploaded images according to the files in array       
+      
+	  if (($_FILES["file"]["size"][$i] < 100000) //Approx. 100kb files can be uploaded.
+                && in_array($file_extension, $validextensions)) {
+            if (move_uploaded_file($_FILES['file']['tmp_name'][$i], $target_path)) {//if file moved to uploads folder
+                echo $j. ').<span id="noerror">Image uploaded successfully!.</span><br/><br/>';
+            } else {//if file was not moved.
+                echo $j. ').<span id="error">please try again!.</span><br/><br/>';
+            }
+        } else {//if file size and file type was incorrect.
+            echo $j. ').<span id="error">***Invalid file Size or Type***</span><br/><br/>';
+        }
+    }
+}
+?>
