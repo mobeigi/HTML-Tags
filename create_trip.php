786027570099fdb5c4ea5f7d630668407d33f955
Antheny <oringalyu@hotmail.com>
2014-10-21 01:26:42 +1100

    .

--- create_trip.php
+++ create_trip.php
@@ -142,7 +142,7 @@
     <h2 style="display:inline-flex">Image Groups</h2> 
         <button type="button" id="CreateImageHelp" class="btn btn-default btn-xs" data-toggle="tooltip" data-placement="right" title="Help for image group"> 
             <script>$('#CreateImageHelp').tooltip();</script>
-            <p><sup>?</sup></p>
+            <p><strong>?</strong></p>
         </button>
     <button style="float:right;margin-top:45px;"type="push" class="btn btn-default">Import Image Group</button>
     <hr>
