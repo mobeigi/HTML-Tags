5dfe553fb630cca22a470575ab31b72582a4be09
Antheny <oringalyu@hotmail.com>
2014-10-20 19:37:57 +1100

    if user has logged in, register link to shows up, changed to show profile link

--- includes/navbar.php
+++ includes/navbar.php
@@ -17,11 +17,11 @@
  <!--        Username and password and navbar links-->
 			<div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1">
 				 <ul class="nav navbar-nav">
-                      <?php if (isset($loggedIn) && ($loggedIn == true)) { ?>
-					       <li><a href="http://triptags.azurewebsites.net/profile.php">Profile</a></li>
-                      <?php } else { 
+                      <?php if (isset($_SESSION['user_id'])){ ?>
+					       print '<li><a href="http://triptags.azurewebsites.net/profile.php">Profile</a></li>';
+                      <?php } else { ?> 
                     print '<li><a href="http://triptags.azurewebsites.net/register.php">Register</a></li>';
-                      }?>
+                   <?php   }?>
 					  <li><a href="#">Tutorial</a></li>
 					  <li><a href="#">Help</a></li>
 					  <li><a href="#">Contact</a></li>
