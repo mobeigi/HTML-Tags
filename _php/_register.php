454990357a83c9e59c5766355b1d2629166afa95
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-20 14:41:15 +1100

    .

--- _php/_register.php
+++ _php/_register.php
@@ -3,6 +3,6 @@ include('_session.php');
 $username = $_POST['userEmail'];
 $password = $_POST['userPassword'];
 
-$query = 'insert into users (user_email,user_password) values ($1,$2)'
+$query = 'insert into users (user_email,user_password) values ($1,$2)';
 $pg->_pg_query($query,$username,$password);
 ?>
