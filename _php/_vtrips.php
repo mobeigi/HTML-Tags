e8271ae63af87b774a1a9d8a439f006397390b36
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-20 13:00:43 +1100

    .

--- _php/_vtrips.php
+++ _php/_vtrips.php
@@ -6,7 +6,7 @@ $rows = $pg->_pg_fetch_all($result);
 $num_rows = $pg->_pg_num_rows($result);
 print $num_rows;
 //print_r($rows);
-print '<table border=1>'
+print '<table border=1>';
 for($i=0;$i!=$num_rows;$i++){
 	foreach(array_keys($rows[$i]) as $key) {
 		print '<tr><td>';
