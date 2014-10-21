9ebc409f5a4113699ff6c68f77144e03b96676ab
mohammadg <webmaster@mohammadg.com>
2014-10-21 22:19:04 +1100

    Added create trip image

--- maps/tripview.php
+++ maps/tripview.php
@@ -12,8 +12,13 @@
 */
 
 //Create database calls and collect required data
-
 include_once('_php/_session.php');
+
+//Collect 1st image (cover image) from each image group in proper order
+//Also collect longitude, latitude
+$query = "select group_id, longitude from image_groups";
+$result = $pg->_pg_query($query);
+
 
 ?>
 
