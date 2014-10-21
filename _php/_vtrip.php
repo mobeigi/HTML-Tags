bee15add8eeadcd5c72c33b4d77b8da0a0f375af
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-22 02:01:01 +1100

    ..

--- _php/_vtrip.php
+++ _php/_vtrip.php
@@ -9,11 +9,11 @@ $latitude = array();
 $group_id = array();
 $images = array();
 
-$query = "select group_id, longitude, latitude from image_groups where trip_id = $1";
+$query = 'select group_id, longitude, latitude from image_groups where trip_id = $1';
 $result = $pg->_pg_query($query, $trip_id);
 
 $row = pg_fetch_all($result);
-$row_nums = sizeof($row) - 1;
+$row_nums = sizeof($row);
 print($row_nums);
 
 for($i = 0; $i != $row_nums; $i++) {
