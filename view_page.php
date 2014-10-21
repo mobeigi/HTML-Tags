47ba8635ca7de69bc715873ca05126c0b549c20b
zychreo95 <cool.ian.w@hotmail.com>
2014-10-21 21:33:37 +1100

    debug

--- view_page.php
+++ view_page.php
@@ -36,31 +36,25 @@
             <!-- hidden link block -->
             <div id="hiddenTripTags" style="visibility:hidden;"></div>
     	</section>
-    
         
-    <!-- Button trigger share link modal -->
-    <button class="btn btn-default" data-toggle="modal" data-target="#shareTripModal">
-      Share Trip
-    </button>
-        
-    <!-- Share link modal window -->
-    <div class="modal fade" id="shareTripModal" tabindex="-1" role="dialog" aria-labelledby="shareTripModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-            <h4 class="modal-title" id="shareTripModalLabel">Trip Link</h4>
-          </div>
-          <div class="modal-body">
-              <p>Give the following link to your friends:</p>
-              <input type="text" id="trip_name" value="http://triptags.azurewebsites.net/view_page.php?trip=19292" style="width: 100%; padding: 5px" readonly>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <!-- Share link modal window -->
+        <div class="modal fade" data-target="shareTripModal" tabindex="-1" role="dialog" aria-labelledby="shareTripModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title" id="shareTripModalLabel">Trip Link</h4>
+              </div>
+              <div class="modal-body">
+                  <p>Give the following link to your friends:</p>
+                  <input type="text" id="trip_name" value="http://triptags.azurewebsites.net/view_page.php?trip=19292" style="width: 100%; padding: 5px" readonly>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
         
          <!-- include footer -->
         <?php include_once "/includes/footer.php"; ?>
