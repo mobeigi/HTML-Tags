566ecac907f13f3b9e2ab07d1b28d0a2f7e907f1
zychreo95 <cool.ian.w@hotmail.com>
2014-10-22 00:44:11 +1100

    Added some formatting

--- create_trip.php
+++ create_trip.php
@@ -107,21 +107,33 @@
    <?php include_once "/includes/navbar.php"; ?>
     
 <!-- Redirect user if they're not logged in -->
-<?php if (!isset($_SESSION['user_id'])) {
-    //Add some spacing
-    for ($x = 0; $x < 2; $x++) {
-        print '<br>';
-    }
+<?php if (!isset($_SESSION['user_id'])) { ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
 
-    //Print error message
-    print '<p><b>Oops! You must be logged in to view this page!</b></p>';
-    print '<p>You can log in through the menu bar in the top-right corner.</p>';
-    print '<a href="http://triptags.azurewebsites.net">Return to home page</a>';
+            <?php 
+            //Add some spacing
+            for ($x = 0; $x < 2; $x++) {
+                print '<br>';
+            }
 
-    //Add some spacing
-    for ($x = 0; $x < 5; $x++) {
-        print '<br>';
-    }
+            //Print error message
+            print '<p><b>Oops! You must be logged in to view this page!</b></p>';
+            print '<p>You can log in through the menu bar in the top-right corner.</p>';
+            print '<a href="http://triptags.azurewebsites.net">Return to home page</a>';
+
+            //Add some spacing
+            for ($x = 0; $x < 5; $x++) {
+                print '<br>';
+            }
+
+            ?>
+            </div>
+            <div class="col-md-2"></div>
+        </div>
+    </div>
 
 } else {?>
     
