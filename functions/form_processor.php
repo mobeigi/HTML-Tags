26500c27ae97d7d9a0ad33ebd9de107e9bc2c00d
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-21 15:18:07 +1100

    .

--- functions/form_processor.php
+++ functions/form_processor.php
@@ -4,6 +4,6 @@ $image_array_size = sizeof($_POST['image_group_name']);
 
 print "<p>$image_array_size</p>";
 for($i = 0; $i != $image_array_size; $i++) {
-    print "<p>$_POST['image_group_name'][$i]</p>";
+    print "<p>".$_POST['image_group_name'][$i]."</p>";
 }
 ?>
