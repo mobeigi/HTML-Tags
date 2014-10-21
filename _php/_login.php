70ee0682f98e2f1e40249a996632c3cea33ddfbf
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-21 17:18:58 +1100

    fixed typo

--- _php/_login.php
+++ _php/_login.php
@@ -7,6 +7,6 @@ $password = $_POST['userPassword'];
 if(!login($username, $password)) {
 		header("location: http://triptags.azurewebsites.net/");
 } else {
-		header("location: http://triptags.azurewebsites/profile.php");
+		header("location: http://triptags.azurewebsites.net/profile.php");
 }
 ?>
