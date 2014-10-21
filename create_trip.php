ac36646a9cf735099e16077aa7b9a05f51bb3f30
Antheny <oringalyu@hotmail.com>
2014-10-21 17:30:41 +1100

    .

--- create_trip.php
+++ create_trip.php
@@ -161,7 +161,7 @@
     <!-- Select Cover photo for the trip -->
     <h2 style="display:inline-flex">Cover Photo</h2>
     <button type="button" id="CoverPhotoHelp" class="btn btn-default btn-xs" data-toggle="tooltip" data-placement="right" title="Image Groups contain photographs of a general location to highlight the experiences you want to share."> 
-            <script>$('#CreateImageHelp').tooltip();</script>
+            <script>$('#CoverPhotoHelp').tooltip();</script>
                 <strong>?</strong>
         </button>
     <hr>
