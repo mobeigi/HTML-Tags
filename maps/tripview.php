f69e3976ed73df08f129651f13dbdcf39219e0f7
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-22 02:09:48 +1100

    ..

--- maps/tripview.php
+++ maps/tripview.php
@@ -12,36 +12,31 @@
 */
 
 //Create database calls and collect required data
-include_once('_php/_session.php');
+include_once('/../_php/_session.php');
+
+$trip_id = $_GET['trip'];
 
 $longitude = array();
 $latitude = array();
 $group_id = array();
+$images = array();
 
-
-//Collect 1st image (cover image) from each image group in proper order
-//Also collect longitude, latitude
-$query = "select group_id, longitude, latitude from image_groups where trip_id = $1";
+$query = 'select group_id, longitude, latitude from image_groups where trip_id = $1';
 $result = $pg->_pg_query($query, $trip_id);
 
-$row = pg_fetch_all($result);
-$row_nums = pg_num_rows($row);
+$rows = pg_fetch_all($result);
+$row_nums = sizeof($rows);
 
 for($i = 0; $i != $row_nums; $i++) {
-  array_push($longitude, $row[$i]['longitude']);
-  array_push($latitude, $row[$i]['latitude']);
-  array_push($group_id, $row[$i]['group_id']);
-}
-
-$query = "select image_id, path from images where group_id = $1";
-$images = array(array());
-for($i = 0; $i != $row_nums; $i++) {
+  array_push($group_id, $rows[$i]['group_id']);
+  array_push($longitude, $rows[$i]['longitude']);
+  array_push($latitude, $rows[$i]['latitude']);
+  //
+  $query = 'select path from images where group_id = $1';
   $result = $pg->_pg_query($query, $group_id[$i]);
-  $row = pg_fetch_all($result);
-  $image_nums = pg_num_rows($row)
+  $row = pg_fetch_assoc($result);
+  array_push($images, $row['path']);
 }
-
-
 
 ?>
 
