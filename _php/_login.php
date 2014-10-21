17a9db30e18263eb63f897f828636a2e111b4564
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-21 15:08:59 +1100

    ..

---
+++ _php/_login.php
@@ -0,0 +1,12 @@
+<?php
+include('_session.php');
+include('_functions.php')
+$username = $_POST['userEmail'];
+$password = $_POST['userPassword'];
+
+if(!login($username, $password)) {
+		header("location: http://triptags.azurewebsites.net/");
+} else {
+		header("location: http://triptags.azurewebsites/profile.php");
+}
+?>
