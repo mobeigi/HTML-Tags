d93c88af89463106c0629bda0b433d74fc0e0e26
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-21 16:36:17 +1100

    ...

--- _php/_session.php
+++ _php/_session.php
@@ -3,6 +3,7 @@
 // Website Session Handler
 //
 include('_postgres.php');
+global $pg;
 $pg = new postgres();
 
 $result = $pg->_pg_connect('23.102.176.176', 'azureuser', 'RLSfTv3Ewx', 'test', 5432);
