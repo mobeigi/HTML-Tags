33bf9c317cbd07b14a7a4dccd784442ef260b498
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-22 00:40:05 +1100

    .

--- _php/_vtrip.php
+++ _php/_vtrip.php
@@ -7,10 +7,8 @@ $trip_id = $_GET['trip'];
 $longitude = array();
 $latitude = array();
 $group_id = array();
+$images = array();
 
-
-//Collect 1st image (cover image) from each image group in proper order
-//Also collect longitude, latitude
 $query = "select group_id, longitude, latitude from image_groups where trip_id = $1";
 $result = $pg->_pg_query($query, $trip_id);
 
@@ -23,17 +21,16 @@ for($i = 0; $i != $row_nums; $i++) {
   array_push($longitude, $row[$i]['longitude']);
   array_push($latitude, $row[$i]['latitude']);
   array_push($group_id, $row[$i]['group_id']);
+  $query = "select path from images where group_id = $1";
+  for($i = 0; $i != $row_nums; $i++) {
+    $pg->_pg_query($query, $group_id[$i]);
+    $row = pg_fetch_assoc($result);
+    array_push($images, $row['path']);
+  }
 }
 
 print_r($group_id);
 
-$query = "select path from images where group_id = $1";
-$images = array();
-for($i = 0; $i != $row_nums; $i++) {
-  $pg->_pg_query($query, $group_id[$i]);
-  $row = pg_fetch_assoc($result);
-  array_push($images, $row['path']);
-}
 
 print_r($images);
 
