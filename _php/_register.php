3a2d2b83e8cdb6c7c8e194951dbfccf60b1e0891
Jason Ng <jsnng@users.noreply.github.com>
2014-10-24 10:54:58 +1100

    added code to stop duplicate usernames

--- _php/_register.php
+++ _php/_register.php
@@ -4,6 +4,11 @@ include_once('_functions.php');
 
 $username = $_POST['userEmail'];
 $password = $_POST['userPassword'];
+$query = 'select user_email from users where user_email = $1';
+$result = $pg->_pg_query($query, $username);
+if($pg->_pg_num_rows($result) > 0) {
+	header("location: http://triptags.azurewebsites.net/");
+}
  if(!empty($username) || !empty($password)) {
 	$query = 'insert into users (user_email,user_password) values ($1,$2)';
 	$pg->_pg_query($query,$username,$password);
