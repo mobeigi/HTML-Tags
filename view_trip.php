06cc99ae0ee39a381dcd3d55fb1c4dd0aa3ca2cd
Jason Ng <jsnng@users.noreply.github.com>
2014-10-24 11:50:34 +1100

    added trip privacy validation

--- view_trip.php
+++ view_trip.php
@@ -1,5 +1,17 @@
 <?php
+include_once('_php/_session.php');
 $tripID = htmlspecialchars($_GET["trip"]);
+
+$query = 'select privacy, owner_id from trips where trip_id = $1';
+$result = $pg->_pg_query($query, $trip_id);
+$row = pg_fetch_assoc($result);
+if(strcmp($row['privacy'], 'onlyme') == 0) {
+	if(!isset($_SESSION['user_id']) || $_SESSION['user_id'] == $row['owner_id']) {
+		// do something here
+		exit();
+	} 
+}
+
 ?>
 
 <!DOCTYPE html >
