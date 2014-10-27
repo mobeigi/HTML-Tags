74b5928e4f02d0c236bd0e2f25bc507a107864bb
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-28 01:04:14 +1100

    fixed issue with privacy settings

--- view_trip.php
+++ view_trip.php
@@ -10,9 +10,9 @@ print_r($row);
 
 if(strcmp($row['privacy'], 'onlyme') == 0) {
 	if(!isset($_SESSION['user_id'])
-		header('location: https://www.google.com.au');
+		header('location: http://triptags.azurewebsites.net/');
 	if($_SESSION['user_id'] != $row['owner_id'])
-		header('location: https://www.google.com.au');
+		header('location: http://triptags.azurewebsites.net/profile.php');
 }
 
 ?>
