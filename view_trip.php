5093384a3ccfd990735fdd5b93fc26725b39399d
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-27 21:30:56 +1100

    .

--- view_trip.php
+++ view_trip.php
@@ -5,11 +5,14 @@ $tripID = htmlspecialchars($_GET["trip"]);
 $query = 'select privacy, owner_id from trips where trip_id = $1';
 $result = $pg->_pg_query($query, $trip_id);
 $row = pg_fetch_assoc($result);
+
+print_r($row);
+
 if(strcmp($row['privacy'], 'onlyme') == 0) {
 	if(!isset($_SESSION['user_id']) || $_SESSION['user_id'] == $row['owner_id']) {
 		// do something here
 		exit();
-	} 
+	}
 }
 
 ?>
@@ -18,16 +21,16 @@ if(strcmp($row['privacy'], 'onlyme') == 0) {
 <html lang="en" ng-app>
   <head>
      <title>TripTags | View trip #<?php echo $tripID; ?></title>
-     
+
      <?php include_once("includes/header.php"); ?>
-     
+
     <style>
       body {
           padding-top: 50px;
           padding-bottom: 20px;
       }
     </style>
-        
+
      <style>
       #map-canvas {
       position: absolute;
@@ -42,9 +45,9 @@ if(strcmp($row['privacy'], 'onlyme') == 0) {
     <!-- insert maps code -->
     <?php include_once("maps/tripview.php"); ?>
     <?php include_once('/maps/imagegroupview.php'); ?>
-    
+
   </head>
-    
+
     <body>
       <!-- insert navbar -->
       <?php $tripViewer = true; ?>
@@ -55,7 +58,7 @@ if(strcmp($row['privacy'], 'onlyme') == 0) {
             <!-- hidden link block -->
             <div id="hiddenTripTags" style="visibility:hidden;"></div>
     	</section>
-        
+
         <!-- Share link modal window -->
         <div class="modal fade" id="shareTripModal" tabindex="-1" role="dialog" aria-labelledby="shareTripModalLabel" aria-hidden="true">
           <div class="modal-dialog">
@@ -68,15 +71,15 @@ if(strcmp($row['privacy'], 'onlyme') == 0) {
                 <p>Give the following link to your friends:</p>
                 <input type="text" id="trip_name" value="http://triptags.azurewebsites.net/view_page.php?trip=19292" style="width: 100%; padding: 5px" readonly>
               </div>
-                
+
               <div class="modal-footer">
                   <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
               </div>
             </div>
           </div>
         </div>
-        
+
          <!-- include footer -->
         <?php include_once "/includes/footer.php"; ?>
-    </body>          
+    </body>
 </html>
