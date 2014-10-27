75fbd55efb1f61298c0418d7e1cef0993de16787
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-28 01:29:34 +1100

    .

--- view_trip.php
+++ view_trip.php
@@ -5,7 +5,14 @@ $query = 'select privacy, owner_id from trips where trip_id = $1';
 $result = $pg->_pg_query($query, $trip_id);
 $row = pg_fetch_assoc($result);
 
-
+if(strcmp($row['privacy'], 'onlyme') == 0) {
+	if(!isset($_SESSION['user_id']) {
+		header('location: http://triptags.azurewebsites.net/');
+	}
+	else if(!$_SESSION['user_id'] == $row['owner_id']) {
+		header('location: http://triptags.azurewebsites.net/profile.php');
+	}
+}
 
 ?>
 
