2a4ad4c80b815d0a05f17d4e59dbbaab4a054b70
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-28 11:52:02 +1100

    .

--- view_trip2.php
+++ view_trip2.php
@@ -1,6 +1,7 @@
 <?php
 include_once('_php/_session.php');
 $tripID = htmlspecialchars($_GET["trip"]);
+
 $query = 'select privacy, owner_id from trips where trip_id = $1';
 $result = $pg->_pg_query($query, $trip_id);
 $row = pg_fetch_assoc($result);
@@ -8,10 +9,8 @@ $row = pg_fetch_assoc($result);
 
 print_r($row);
 if(strcmp($row['privacy'], 'onlyme') == 0) {
-    if(!isset($_SESSION['user_id']))
-      print '!isset($_SESSION[\'user_id\'])';
-    else if(!($_SESSION['user_id'] == $row['owner_id']))
-    print '!($_SESSION[\'user_id\'] == $row[\'owner_id\']';
+    if(!isset($_SESSION['user_id'])) print 1;
+    else if(!($_SESSION['user_id'] == $row['owner_id'])) print 2;
 }
 
 ?>
