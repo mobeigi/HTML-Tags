8cd28ae73ec429651bcf031932df87f941953fc4
Jason Ng <jsfn660@cse.unsw.edu.au>
2014-10-28 11:57:14 +1100

    .

--- view_trip2.php
+++ view_trip2.php
@@ -5,7 +5,8 @@ $tripID = htmlspecialchars($_GET["trip"]);
 $query = 'select privacy, owner_id from trips where trip_id = $1';
 $result = $pg->_pg_query($query, $trip_id);
 $row = pg_fetch_assoc($result);
-
+print $row['privacy'];
+print $row['owner_id'];
 
 print_r($row);
 if(strcmp($row['privacy'], 'onlyme') == 0) {
